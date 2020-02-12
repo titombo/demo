8d2b9c2ee661852e9ef36ffb9a1cc6bec2f69d4d
Morais Brito Oliveira <tito.morais-brito-oliveira@capgemini.com>
2020-02-12 08:40:40 +0100

    changing the message

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace demo
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World 2!");
             });
         }
     }
